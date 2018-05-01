3d26303b0b239664a04e9dc872cdf57efa2d0f40
Jason Stoltzfus <jastoltz24@gmail.com>
2018-05-01 15:30:33 -0400

    Initial UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,59 @@
 import React, { Component } from "react";
-import "./App.css";
+import { Input } from "rebass";
+import styled from "styled-components";
+
+const HeaderBar = styled.div`
+  color: rgb(255, 255, 255);
+  background-color: rgba(0, 0, 0, 0.75);
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  padding: 1em;
+`;
+
+const HeaderBarInner = styled.div`
+  width: 100%;
+  max-width: 1024px;
+  display: flex;
+`;
+
+const StyledInput = Input.extend`
+  background-color: white;
+  flex: 1;
+  color: black;
+`;
+
+const Title = styled.div`
+  margin-right: 20px;
+  font-size: 2em;
+`;
+
+const Body = styled.div`
+  display: flex;
+  justify-content: center;
+  padding-top: 20px;
+`;
+
+const BodyInner = styled.div`
+  width: 100%;
+  max-width: 1024px;
+`;
 
 class App extends Component {
   render() {
-    return <div className="App">Search</div>;
+    return (
+      <div className="App">
+        <HeaderBar>
+          <HeaderBarInner>
+            <Title>Node Module Search</Title>
+            <StyledInput />
+          </HeaderBarInner>
+        </HeaderBar>
+        <Body>
+          <BodyInner>Results go here</BodyInner>
+        </Body>
+      </div>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import { injectGlobal } from "styled-components";
+
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
+
+injectGlobal`
+  * { box-sizing: border-box; }
+  body { margin: 0; }
+`;
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
